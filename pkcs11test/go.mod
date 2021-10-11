23ff11eeef57dbfa7f23de68c85ba5699937682e
hxy <xueyanghan@tencent.com>
2021-10-11 11:20:48 +0800

    softhsm2 test

---
+++ pkcs11test/go.mod
@@ -0,0 +1,8 @@
+module chainmaker.org/gotest/pkcs11test
+
+go 1.16
+
+require (
+	github.com/miekg/pkcs11 v1.0.3
+	github.com/stretchr/testify v1.7.0
+)
---
+++ pkcs11test/pkcs11_test.go
@@ -0,0 +1,49 @@
+package pkcs11test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/miekg/pkcs11"
+)
+
+//softhsm2-util --init-token --slot 0 --label test --pin 1234
+
+func TestNew(t *testing.T) {
+	_ = pkcs11.New("/usr/local/Cellar/softhsm/2.6.1/lib/softhsm/libsofthsm2.so")
+}
+
+func TestSHA1(t *testing.T) {
+	p := pkcs11.New("./libs/libsofthsm2.so")
+	assert.NotNil(t, p)
+
+	err := p.Initialize()
+	assert.NoError(t, err)
+
+	defer p.Destroy()
+	defer p.Finalize()
+
+	slots, err := p.GetSlotList(true)
+	assert.NoError(t, err)
+	assert.NotNil(t, slots) //must initialize softhsm locally. CMD: softhsm2-util --init-token --slot 0 --label test --pin 1234
+
+	session, err := p.OpenSession(slots[0], pkcs11.CKF_SERIAL_SESSION|pkcs11.CKF_RW_SESSION)
+	assert.NoError(t, err)
+	defer p.CloseSession(session)
+
+	err = p.Login(session, pkcs11.CKU_USER, "1234")
+	assert.NoError(t, err)
+
+	defer p.Logout(session)
+
+	p.DigestInit(session, []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_SHA_1, nil)})
+	hash, err := p.Digest(session, []byte("this is a string"))
+	assert.NoError(t, err)
+
+	for _, d := range hash {
+		fmt.Printf("%x", d)
+	}
+	fmt.Println()
+}
