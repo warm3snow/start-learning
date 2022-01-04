0ea728719d79e38b479b0f263ad33e64387e2abc
hxy <xueyanghan@tencent.com>
2022-01-04 20:53:01 +0800

    warm3snow tjfoc test

--- gmssltest/tjfoctest/warm3snow_tjfoc_test.go
+++ gmssltest/tjfoctest/warm3snow_tjfoc_test.go
@@ -3,8 +3,9 @@ package gmcredentials
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -16,12 +17,50 @@ import (
 
 var (
 	caCert = "../tassl_demo_certs/CA.crt"
+
+	ssCert = "../tassl_demo_certs/SS.crt"
+	ssKey  = "../tassl_demo_certs/SS.key"
+	seCert = "../tassl_demo_certs/SE.crt"
+	seKey  = "../tassl_demo_certs/SE.key"
+
 	csCert = "../tassl_demo_certs/CS.crt"
 	csKey  = "../tassl_demo_certs/CS.key"
-
 	ceCert = "../tassl_demo_certs/CE.crt"
 	ceKey  = "../tassl_demo_certs/CE.key"
 )
+
+func TestGmServer(t *testing.T) {
+	// 切换GMSSL/TSL
+	sigCert, err := gmtls.LoadX509KeyPair(ssCert, ssKey)
+	assert.NoError(t, err)
+	encCert, err := gmtls.LoadX509KeyPair(seCert, seKey)
+	assert.NoError(t, err)
+
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM([]byte(caCert))
+
+	config := &gmtls.Config{
+		GMSupport:    &gmtls.GMSupport{},
+		Certificates: []gmtls.Certificate{sigCert, encCert},
+		RootCAs:      certPool,
+	}
+
+	ln, err := gmtls.Listen("tcp", ":44330", config)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ln.Close()
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "hello\n")
+	})
+	fmt.Println(">> HTTP Over [GMSSL/TLS] running...")
+	err = http.Serve(ln, nil)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // gmGCMClientRun GCM模式测试
 func TestGmClient(t *testing.T) {
@@ -51,12 +90,29 @@ func TestGmClient(t *testing.T) {
 	assert.NoError(t, err)
 	defer conn.Close()
 
-	for i := 0; i < 10; i++ {
-		req := []byte(fmt.Sprintf("hello chainmaker%d\n", i))
-		_, _ = conn.Write(req)
+	//this commented code is used to test tassl server
+	//for i := 0; i < 10; i++ {
+	//	req := []byte(fmt.Sprintf("hello chainmaker%d\n", i))
+	//	_, _ = conn.Write(req)
+	//
+	//	time.Sleep(time.Second * 3)
+	//}
 
-		time.Sleep(time.Second * 3)
+	req := []byte("GET / HTTP/1.1\r\n" +
+		"Host: localhost\r\n" +
+		"Connection: close\r\n\r\n")
+	conn.Write(req)
+
+	buff := make([]byte, 1024)
+	for {
+		n, _ := conn.Read(buff)
+		if n <= 0 {
+			break
+		} else {
+			fmt.Printf("%s", buff[0:n])
+		}
 	}
+
 	fmt.Println(">> SM2_SM4_GCM_SM3 suite [PASS]")
 }
 
