bab75115ca21ee51a793af17e5ff3462b1f9688c
hxy <xueyanghan@tencent.com>
2022-01-18 17:35:32 +0800

    add ecdsa verify benchamark

--- cryptotest/tjfoc_test.go
+++ cryptotest/tjfoc_test.go
@@ -1,9 +1,14 @@
 package cryptotest
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
+	gsm2 "chainmaker.org/chainmaker/common/v2/opencrypto/gmssl/sm2"
 	"github.com/tjfoc/gmsm/sm2"
 )
 
@@ -23,5 +28,28 @@ func BenchmarkTjfocSM2_Verify(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		priv.PublicKey.Verify(msg, sig)
+	}
+}
+
+func BenchmarkEcdsaP256_Verify(b *testing.B) {
+	priv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	sig, _ := priv.Sign(rand.Reader, msg, nil)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok := ecdsa.VerifyASN1(&priv.PublicKey, msg, sig)
+		assert.True(b, ok)
+	}
+}
+
+func BenchmarkGmsslSM2_verify(b *testing.B) {
+	priv, _ := gsm2.GenerateKeyPair()
+	sig, _ := priv.Sign(msg)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok, err := priv.PublicKey().Verify(msg, sig)
+		assert.NoError(b, err)
+		assert.True(b, ok)
 	}
 }
