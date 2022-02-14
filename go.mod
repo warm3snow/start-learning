9fbdcc7db4bd614b63a7373376effc5f81a38e63
hxy <xueyanghan@tencent.com>
2022-02-14 12:04:41 +0800

    httpstest for chainmaker-go

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module chainmaker.org/gotest
 go 1.16
 
 require (
-	chainmaker.org/chainmaker/common/v2 v2.1.2-0.20220107095900-8f97311ecc8d
+	chainmaker.org/chainmaker/common/v2 v2.1.2-0.20220127064703-4da19a869e9d
 	github.com/Hyperledger-TWGC/tjfoc-gm v1.4.1-0.20210806094929-4b29e6230155
 	github.com/golang/protobuf v1.5.2
 	github.com/miekg/pkcs11 v1.0.3
 	github.com/spacemonkeygo/openssl v0.0.0-20181017203307-c2dcc5cca94a
 	github.com/stretchr/testify v1.7.0
 	github.com/tjfoc/gmsm v1.4.2-0.20211201010834-02e00ad77028
-	github.com/warm3snow/gmsm v0.0.0-20211230075704-06afd4a5ba8c // indirect
+	github.com/warm3snow/gmsm v0.0.0-20211230075704-06afd4a5ba8c
 	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420
 	google.golang.org/grpc v1.40.0
 )
--- httpstest/rpcserver_https_test.go
+++ httpstest/rpcserver_https_test.go
@@ -1,16 +1,16 @@
 package httpstest
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	cmtls "chainmaker.org/chainmaker/common/v2/crypto/tls"
+	cmhttp "chainmaker.org/chainmaker/common/v2/crypto/tls/http"
 	cmx509 "chainmaker.org/chainmaker/common/v2/crypto/x509"
 )
 
@@ -60,22 +60,21 @@ func TestChainmaker_rpcserver_GM_https_getversion(t *testing.T) {
 	}
 }
 
-func TestChainmaker_rpcserver_https_getversion(t *testing.T) {
+func TestGetversion1(t *testing.T) {
 	// 信任的根证书
-	certPool := x509.NewCertPool()
+	certPool := cmx509.NewCertPool()
 	cacert, err := ioutil.ReadFile(caCert)
 	assert.NoError(t, err)
 	certPool.AppendCertsFromPEM(cacert)
 
-	cert, err := tls.LoadX509KeyPair(adminTlsCrt, adminTlsKey)
+	cert, err := cmtls.LoadX509KeyPair(adminTlsCrt, adminTlsKey)
 
-	config := &tls.Config{
-		//GMSupport:    &cmtls.GMSupport{},
+	config := &cmtls.Config{
 		RootCAs:      certPool,
-		Certificates: []tls.Certificate{cert},
+		Certificates: []cmtls.Certificate{cert},
 	}
 
-	conn, err := tls.Dial("tcp", "localhost:12301", config)
+	conn, err := cmtls.Dial("tcp", "localhost:12301", config)
 	if err != nil {
 		panic(err)
 	}
@@ -90,4 +89,28 @@ func TestChainmaker_rpcserver_https_getversion(t *testing.T) {
 	n, err := conn.Read(buff)
 	assert.NoError(t, err)
 	log.Println(string(buff[:n]))
+}
+
+func TestVersion2(t *testing.T) {
+	config, err := cmhttp.GetConfig(adminTlsCrt, adminTlsKey, caCert, false)
+	assert.NoError(t, err)
+	//config.ServerName = "server sign (SM2)"
+
+	client := cmhttp.NewClient(config)
+	resp, err := client.Get("https://localhost:12301/v1/getversion")
+	assert.NoError(t, err)
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	log.Println("receive from server: " + string(buf))
+}
+
+func TestVersion2NotHttps(t *testing.T) {
+	//config.ServerName = "server sign (SM2)"
+	resp, err := http.Get("https://localhost:12301/v1/getversion")
+	assert.NoError(t, err)
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	log.Println("receive from server: " + string(buf))
 }
